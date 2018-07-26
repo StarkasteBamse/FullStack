b3a58b1da1f5ae4a70d7172bf92383c135f19e15
StarkasteBamse <sulevi.sihvola@gmail.com>
2018-07-26 17:32:05 +0300

    1.12

---
+++ Osa1/anekdootit/src/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: 0
+    }
+  }
+
+  vaihda = () => {
+    this.setState({selected: getRandomInt(0,5)})
+  }
+
+
+  render() {
+    
+    return (
+      <div>
+        {this.props.anecdotes[this.state.selected]}
+        <br/>
+        <button onClick={this.vaihda}>nappi</button>
+      </div>
+    )
+  }
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)

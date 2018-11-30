46298ef2781ef0ada05bccd11978c2793271ae2b
StarkasteBamse <sulevi.sihvola@gmail.com>
2018-11-30 13:54:54 +0200

    4.5

--- Osa4/utils/list_helper.js
+++ Osa4/utils/list_helper.js
@@ -5,7 +5,13 @@ const totalLikes = (blogs) => {
     return blogs.reduce(((sum, blog) => {return sum + blog.likes}), 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {return 0}
+    return blogs.reduce(((first, blog) => {return first.likes > blog.likes? first : blog}), blogs[0])
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
